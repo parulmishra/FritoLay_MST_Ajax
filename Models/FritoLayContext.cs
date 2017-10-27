46b872f5d9d28b3a428726643150be964f5999b9
Parul Mishra <parul.mishra78@gmail.com>
2017-10-27 16:51:01 -0700

    final commit

---
+++ Models/FritoLayContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FritoLay.Models
+{
+    public class FritoLayContext: IdentityDbContext<User>
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=3306;database=FritoLay;uid=parul;pwd=parul;");
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FritoLay.Models
+{
+    [Table("Products")]
+    public class Product : IEquatable<Product>
+    {
+        public Product()
+        {
+            this.Reviews = new HashSet<Review>();
+        }
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Cost { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public bool Featured { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        public bool Equals(Product other)
+        {
+            if (this.ProductId != other.ProductId)
+                return false;
+            if (this.ProductName != other.ProductName)
+                return false;
+            if (this.Featured != other.Featured)
+                return false;
+            if (this.CountryOfOrigin != other.CountryOfOrigin)
+                return false;
+            return true;
+        }
+    }
+}
